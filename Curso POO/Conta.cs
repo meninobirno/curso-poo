4fb430af83d66a2ab7152d9d58457828794859da
Bini <vinicioschubert@hotmail.com>
2021-02-24 17:42:52 -0300

    Exercicio da conta bancaria

---
+++ Curso POO/Conta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso_POO
+{
+    public class Conta
+    {
+        private int IdConta { get; set; }
+        private string Titular { get; set; }
+        private double Saldo { get; set; }
+
+        public void CriarConta(int idConta, string titular, double saldo, bool deposito)
+        {
+            IdConta = idConta;
+            Titular = titular;
+            if(deposito == true)
+            {
+                Saldo = saldo;
+            }
+            else
+            {
+                Saldo = 0.0;
+            }
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {IdConta}, Titular: {Titular}, Saldo: {Saldo}";
+        }
+        
+
+        
+
+
+    }
+}
---
+++ Curso POO/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Curso_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o número da conta: ");
+            int idConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n) ");
+            double saldo = 0.0;
+            bool deposito = true;
+            if(Console.ReadLine() == "n" || Console.ReadLine() == "N")
+            {
+                deposito = false;
+            }
+            else
+            {
+                Console.Write("Entre com o valor do depósito: ");
+                saldo = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("");
+            
+            Conta c = new Conta();
+            c.CriarConta(idConta, titular, saldo, deposito);
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c.ToString());
+
+            Console.Write("Entre com o valor do depósito: ");
+            saldo = double.Parse(Console.ReadLine());
+
+            c.Deposito(saldo);
+            Console.WriteLine("");
+            Console.WriteLine("Dados atualizados da conta:");
+            Console.WriteLine(c.ToString());
+            Console.WriteLine("");
+
+            Console.Write("Entre com o valor para saque: ");
+            saldo = double.Parse(Console.ReadLine());
+
+            c.Saque(saldo);
+            Console.WriteLine("");
+            Console.WriteLine("Dados atualizados da conta:");
+            Console.WriteLine(c.ToString());
+            Console.WriteLine("");
+
+        }
+    }
+}
